bb05a33ba60041c6e5de4aa080d5f16dcc1037cc
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-22 12:33:27 +0630

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,13 +14,13 @@ class UsersTableSeeder extends Seeder
 {
     public function run(): void
     {
-        $admin = $this->createUser(UserType::Admin, 'Owner', 'bossi', '09123456789');
+        $admin = $this->createUser(UserType::Admin, 'Owner', 'shan', '09123456789');
         (new WalletService)->deposit($admin, 10 * 100_000, TransactionName::CapitalDeposit);
 
         $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'A898737', '09112345674', $admin->id, 'vH4HueE9');
         (new WalletService)->transfer($admin, $agent_1, 5 * 100_000, TransactionName::CreditTransfer);
 
-        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'P111111', '09111111111', $agent_1->id);
+        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'Player001', '09111111111', $agent_1->id);
         (new WalletService)->transfer($agent_1, $player_1, 30000, TransactionName::CreditTransfer);
     }
 
