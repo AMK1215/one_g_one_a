1b51e4b9ff28a3ed7b544a5619d8e8410cf8d62d
sophia <thangneimawi@hivelocity.co.jp>
2024-10-26 13:00:22 +0700

    rename title

--- app/Http/Controllers/Admin/Agent/AgentController.php
+++ app/Http/Controllers/Admin/Agent/AgentController.php
@@ -234,7 +234,7 @@ class AgentController extends Controller
     {
         $randomNumber = mt_rand(10000000, 99999999);
 
-        return 'LKM'.$randomNumber;
+        return 'P'.$randomNumber;
     }
 
     public function banAgent($id): RedirectResponse
